fdd6877c11c77eaa08533dd1db1563b5201f8286
Rahman Oridedi <orisdev@gmail.com>
2016-11-08 17:12:48 -0500

    updating web

--- app-common/target/generated-sources/annotations/com/ortech/app/model/Customer_.java
+++ app-common/target/generated-sources/annotations/com/ortech/app/model/Customer_.java
@@ -6,8 +6,9 @@ import javax.persistence.metamodel.StaticMetamodel;
 
 @Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
 @StaticMetamodel(Customer.class)
-public abstract class Customer_ {
+public abstract class Customer_ extends com.ortech.app.model.BaseEntity_ {
 
+	public static volatile SingularAttribute<Customer, Boolean> isMarried;
 	public static volatile SingularAttribute<Customer, String> firstName;
 	public static volatile SingularAttribute<Customer, String> lastName;
 	public static volatile SingularAttribute<Customer, Integer> numberOfAccudents;
