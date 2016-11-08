26fb0105878c0361ed717f8bbc6d0673c322c8fb
Rahman Oridedi <orisdev@gmail.com>
2016-11-08 15:42:36 -0500

    updating dao

---
+++ app-services/src/main/java/com/ortech/app/dao/impl/InsuranceDao.java
@@ -0,0 +1,90 @@
+package com.ortech.app.dao.impl;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import com.ortech.app.dao.BaseDao;
+import com.ortech.app.model.Insurance;
+
+public class InsuranceDao implements BaseDao<Insurance> {
+
+	@Override
+	public void setEntityManager(EntityManager newEntityManager) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public EntityManager getEntityManager() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Insurance get(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Insurance> get(Integer... ids) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Insurance> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Insurance save(Insurance object) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Insurance> save(Insurance... objects) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Integer... ids) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Insurance object) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Insurance... objects) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteAll() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Insurance> findByExample(Insurance object) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
